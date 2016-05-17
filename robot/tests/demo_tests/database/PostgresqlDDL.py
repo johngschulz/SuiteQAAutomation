8c96f326f7e2c2eff27707f6bf649fbab3a0c8a2
DBlasby <dblasby@boundlessgeo.com>
2016-05-17 14:25:02 -0700

    Update PostgresqlDDL.py

--- robot/tests/demo_tests/database/PostgresqlDDL.py
+++ robot/tests/demo_tests/database/PostgresqlDDL.py
@@ -7,13 +7,18 @@ class PostgresqlDDL:
         logger.debug ('Connecting using : %s.connect(database=%s, user=%s, password=%s, host=%s, port=%s) ' % (dbapiModuleName, dbName, dbUsername, dbPassword, dbHost, dbPort))
 
         try:
+            dbconnection =  None
             dbconnection = db_api_2.connect (database=dbName, user=dbUsername, password=dbPassword, host=dbHost, port=dbPort)
             dbconnection.set_isolation_level(0)
             try:
                 cursor = dbconnection.cursor()
                 cursor.execute(ddlCommand)
+            except Exception as e:
+                print "*FAIL* ", e
             finally:
                 cursor.close()
+        except Exception as e:
+                print "*FAIL* ", e
         finally:
-             dbconnection.close()
-        
+             if dbconnection is not None:
+                dbconnection.close()
