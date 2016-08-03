2e97fbebe9486c51b369c445e91980dfe4780f27
Morgan Thompson <mthompson@boundlessgeo.com>
2016-08-03 15:13:18 -0700

    updated WAR script for marlin changes

--- infrastructure/ansible/roles/win_2012/files/install.sh
+++ infrastructure/ansible/roles/win_2012/files/install.sh
@@ -25,10 +25,10 @@ sleep 5
 # "/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-DGEOSERVER_DATA_DIR=c:\ProgramData\Boundless\OpenGeo\geoserver\geoserverDataDir"
 
 #setup marlin, put somewhere other than web-inf and update classpath
-"/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Xbootclasspath/a:C:\Program Files (x86)\Apache Software Foundation\Tomcat 8.0\webapps\geoserver\WEB-INF\lib\marlin-0.7.3-Unsafe.jar"
+"/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Xbootclasspath/a:C:\Program Files (x86)\Boundless\suite\geoserver\marlin\marlin-0.7.3-Unsafe.jar"
 "/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Dsun.java2d.renderer=org.marlin.pisces.PiscesRenderingEngine"
 "/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Dsun.java2d.renderer.useThreadLocal=false"
-"/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Djava.library.path=C:\GDAL';'C:\Program Files (x86)\netCDF 4.4.0\bin';'C:\libjpeg-turbo\bin"
+"/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Djava.library.path=C:\Program Files (x86)\Boundless\suite\geoserver\gdal\';'C:\Program Files (x86)\netCDF 4.4.0\bin';'C:\libjpeg-turbo\bin"
 "/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 8.0/bin/Tomcat8.exe" //US//Tomcat8  ++JvmOptions="-Djna.library.path=C:\Program Files (x86)\netCDF 4.4.0\bin"
 
 
