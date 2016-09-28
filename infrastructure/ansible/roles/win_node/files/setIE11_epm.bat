ec43ffece7c64aaab07ee7f0d818190c42193794
Morgan Thompson <mthompson@boundlessgeo.com>
2016-09-28 13:21:46 -0700

    added bat file to set IE11 protected mode settings during provision

---
+++ infrastructure/ansible/roles/win_node/files/setIE11_epm.bat
@@ -0,0 +1,4 @@
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" /v 2500 /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" /v 2500 /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v 2500 /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4" /v 2500 /t REG_DWORD /d 0 /f
