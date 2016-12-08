9a4daf4ccbc9e1ca691f9314844d29a5c5b003f3
Morgan Thompson <mthompson@boundlessgeo.com>
2016-12-08 14:39:30 -0800

    update to selenium 2.53.1

--- infrastructure/ansible/roles/win_node/files/start_node.bat
+++ infrastructure/ansible/roles/win_node/files/start_node.bat
@@ -1,3 +1,3 @@
  cd C:\cygwin\home\Administrator\
 
- call java -jar "selenium-server-standalone-2.52.0.jar" -role webdriver -hub http://10.0.0.185:4444/grid/register -Dwebdriver.ie.driver="C:/source/IEDriverServer.exe" -DWebdriver.chrome.driver="C:/source/chromedriver.exe" -Dwebdriver.edge.driver="C:/source/MicrosoftWebDriver.exe" -nodeConfig C:\\cygwin\\home\\Administrator\\nodeConfig.json
+ call java -jar "selenium-server-standalone-2.53.1.jar" -role webdriver -hub http://10.0.0.185:4444/grid/register -Dwebdriver.ie.driver="C:/source/IEDriverServer.exe" -DWebdriver.chrome.driver="C:/source/chromedriver.exe" -Dwebdriver.edge.driver="C:/source/MicrosoftWebDriver.exe" -nodeConfig C:\\cygwin\\home\\Administrator\\nodeConfig.json
