5aee7c3cf76711001fe0ddc1931fd175d1d1772d
Morgan Thompson <mthompson@boundlessgeo.com>
2016-12-08 14:41:04 -0800

    remove unsued start_node.sh

--- infrastructure/ansible/roles/win_node/files/start_node.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo /home/Administrator/nssm.exe install SelNode \"C:\\Program Files\\Java\\jdk1.8.0_72\\bin\\java.exe\" "-jar C:\cygwin\home\Administrator\selenium-server-standalone-2.52.0.jar  -role webdriver -hub http://10.0.27.43:4444/grid/register -nodeConfig C:\\cygwin\\home\\Administrator\\files\\nodeConfig.json\"
