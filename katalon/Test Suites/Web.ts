362a99e572b289fe8546060611edd3f149c06ce0
Torben Barsballe <tbarsballe@boundlessgeo.com>
2018-06-28 12:31:15 -0700

    Update web test timing to work from command-line execution

--- katalon/Test Suites/Web.ts
+++ katalon/Test Suites/Web.ts
@@ -4,7 +4,7 @@
    <name>Web</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-03-21T16:01:31</lastRun>
+   <lastRun>2018-06-28T12:13:50</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
